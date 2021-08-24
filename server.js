19d022b221e728feb9ee43f176bc91ebf6603cf9
Md. Jahid Hasan <78964030+hello-jahid@users.noreply.github.com>
2021-08-24 16:24:42 +0600

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const socket = require('socket.io');
 const { ExpressPeerServer } = require('peer');
 const groupCallHandler = require('./groupCallHandler');
 const { v4: uuidv4 } = require('uuid');
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
